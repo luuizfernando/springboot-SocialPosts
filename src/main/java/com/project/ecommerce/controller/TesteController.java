b459d2d2ad60dc790f3c97f340b7482352abfb0d
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-16 14:30:53 -0300

    Project created

---
+++ src/main/java/com/project/ecommerce/controller/TesteController.java
@@ -0,0 +1,12 @@
+package com.project.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+    @GetMapping("/")
+    public String olaMundo() {
+        return "Olá Mundo";
+    }
+}
